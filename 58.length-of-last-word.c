a032337a6c99e7ddf7c1d224aab706b1e3df7a96
Nithin-Shetty-M <mnithinshetty15@gmail.com>
2026-09-23 21:01:23 +0530

    Started with new problem length of last word

---
+++ 58.length-of-last-word.c
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode id=58 lang=c
+ *
+ * [58] Length of Last Word
+ */
+
+// @lc code=start
+int lengthOfLastWord(char* s) {
+  int len=strlen(s);
+  for(i=len-1;i>=0;i--)
+  {
+    
+  }  
+}
+// @lc code=end
+
