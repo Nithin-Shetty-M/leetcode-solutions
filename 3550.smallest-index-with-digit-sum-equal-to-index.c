a13e956e0be464465b19cb0e6260c3b207b6be50
Nithin-Shetty-M <mnithinshetty15@gmail.com>
2026-09-24 22:40:56 +0530

    A new problem

---
+++ 3550.smallest-index-with-digit-sum-equal-to-index.c
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode id=3550 lang=c
+ *
+ * [3550] Smallest Index With Digit Sum Equal to Index
+ */
+
+// @lc code=start
+int smallestIndex(int* nums, int numsSize) {
+    int r,n,i;
+    for(i=0;i<numsSize;i++)
+    {
+        n=nums[i];
+        while(n!=0)
+        {
+            r+=n%10;
+            n=n/10;
+        }
+        if(r==i)
+        return i;
+    }
+    return -1;
+}
+// @lc code=end
+
