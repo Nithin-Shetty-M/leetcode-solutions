19c250a25e1a2695cbb72016765550d83940b455
Nithin-Shetty-M <mnithinshetty15@gmail.com>
2026-09-25 22:29:47 +0530

    trying new problem

---
+++ 28.find-the-index-of-the-first-occurrence-in-a-string.c
@@ -0,0 +1,18 @@
+/*
+ * @lc app=leetcode id=28 lang=c
+ *
+ * [28] Find the Index of the First Occurrence in a String
+ */
+
+// @lc code=start
+int strStr(char* haystack, char* needle) {
+   int i,j,k;
+   for(i=0;i<strlen(haystack);i++)
+   {
+    j=i;
+    k=0;
+    
+   } 
+}
+// @lc code=end
+
