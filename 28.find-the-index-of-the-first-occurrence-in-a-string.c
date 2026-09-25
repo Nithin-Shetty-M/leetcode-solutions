0e3aa946cbb8a3d6c80d49f76b15c985aa3ddd61
Nithin-Shetty-M <mnithinshetty15@gmail.com>
2026-09-25 23:09:19 +0530

    Solved leetcode problem no 5

--- 28.find-the-index-of-the-first-occurrence-in-a-string.c
+++ 28.find-the-index-of-the-first-occurrence-in-a-string.c
@@ -7,12 +7,21 @@
 // @lc code=start
 int strStr(char* haystack, char* needle) {
    int i,j,k;
-   for(i=0;i<strlen(haystack);i++)
+   if(strlen(haystack)<strlen(needle))
+   return -1;
+   for(i=0;i<=strlen(haystack)-(strlen(needle)-2);i++)
    {
     j=i;
     k=0;
-    
+    while(k<strlen(needle) && j<strlen(haystack) && haystack[j]==needle[k] )
+    {
+        j++;
+        k++;
+    }
+    if(strlen(needle)==k)
+    return i;
    } 
+   return -1;
 }
 // @lc code=end
 
